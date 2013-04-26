bd2794c204cb08cadd02c01b66bc6079983e4c3b
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-04-26 15:37:52 -0300

    Include rails project 'agendamento'
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agendamento::Application.config.secret_token = '99e3edd14f2f16c3601028cf6613eff9d7e4f47695603ef76d8c6dc6934469594a6e0f800d186b1c0df91591c76bcec5a4324aeefcc55b231dbd10c666e5096e'
